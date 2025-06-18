b36e711e89fc812cda3ffef0605fccc49b129976
mkebe717 <mkebe7408@gmail.com>
2025-06-18 13:06:03 +0000

    Update algo-1.c

--- algo-1.c
+++ algo-1.c
@@ -18,9 +18,31 @@ float montantMin(Facture tab[], int n) {
 }
 
 //definir la structure facture
-
+Facture = enregistrement
+objet:chaine
+montant: réel
+fin
 //procedure remplir tab
+procédure remplir_tab(var T: tableau de Facture; n: entier)
+var
+  i: entier
+début
+  pour i de 1 à n faire
+    écrire("Entrer l'objet de la facture ", i, ": ")
+    lire(T[i].objet)
+    écrire("Entrer le montant de la facture ", i, ": ")
+    lire(T[i].montant)
+  fin pour
+fin
  //procedure affiche tab
+ procédure affiche_tab(T: tableau de Facture; n: entier)
+var
+  i: entier
+début
+  pour i de 1 à n faire
+    écrire("Facture ", i, ": Objet = ", T[i].objet, ", Montant = ", T[i].montant)
+  fin pour
+fin
 
  //fonction somme par objet 
  FUNCTION sommeParObjet(tab: ARRAY OF Facture; n: INTEGER; objetRecherche: STRING): REAL;
